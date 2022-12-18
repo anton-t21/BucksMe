36c4d00968982ccad7f22a5502a1e7e92514f603
Anton Tihonovich <>
2022-12-18 15:18:04 +0100

    setup project

---
+++ BucksMe/BucksMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucksMeApp.swift
+//  BucksMe
+//
+//  Created by Anton Tsikhanovich on 17/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct BucksMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Podfile
@@ -0,0 +1,14 @@
+platform :ios, '14.0'
+use_frameworks!
+inhibit_all_warnings!
+
+target 'BucksMe' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for BucksMe
+
+  pod 'SwiftLint'
+  pod 'R.swift'
+
+end
